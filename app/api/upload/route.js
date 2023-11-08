892beb0339954f469a3feca69f08771bd11bb301
Attila_Tapai <attila.tapai01@gmail.com>
2023-11-08 20:59:54 +0100

    solved upload cred

--- app/api/upload/route.js
+++ app/api/upload/route.js
@@ -3,9 +3,10 @@ const { Firestore, Timestamp } = require("@google-cloud/firestore");
 const { v4: uuidv4 } = require("uuid");
 
 const credentials = JSON.parse(Buffer.from(process.env.GOOGLE_APPLICATION_CREDENTIALS_BASE64, "base64").toString("utf-8"));
+const projectId = process.env.GCP_PROJECT_ID;
 
 const storage = new Storage({ credentials });
-const db = new Firestore({ credentials });
+const db = new Firestore({ credentials, projectId});
 const bucket = storage.bucket("whats_that_meme_bucket");
 
 export async function POST(req) {
