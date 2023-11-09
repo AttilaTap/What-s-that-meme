5f17544f3fd9e51862b5dbdae30656c07df06e34
Attila_Tapai <attila.tapai01@gmail.com>
2023-11-09 14:56:27 +0100

    next.config.js pwa works

--- next.config.js
+++ next.config.js
@@ -1,16 +1,5 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  compiler: {
-    removeConsole: process.env.NODE_ENV !== 'development',
-  },
-};
-
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
-  register: true,
+const withPWA = require("next-pwa")({
+  dest: "public",
 });
 
-module.exports = withPWA(nextConfig);
+module.exports = withPWA({});
