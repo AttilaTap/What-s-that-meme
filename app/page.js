22fcdea2cc6b8042326cc54e16c4bb3c11eae06f
Attila_Tapai <attila.tapai01@gmail.com>
2023-11-10 14:20:38 +0100

    implement, fix search function

--- app/page.js
+++ app/page.js
@@ -8,7 +8,7 @@ import useLabels from "./hooks/useLabels";
 import useFileDrop from "./hooks/useFileDrop";
 import ImageDropzone from "./components/imageDropzone";
 import algoliasearch from "algoliasearch/lite";
-import { InstantSearch, SearchBox, Hits } from "react-instantsearch";
+import { InstantSearch, SearchBox, Hits, Pagination, Configure } from "react-instantsearch";
 
 export default function Home() {
   const [fileRejections, setFileRejections] = useState([]);
@@ -21,18 +21,36 @@ export default function Home() {
 
   const onDrop = useFileDrop(setUploadedImage, resetAnalysis, resetLabels, setFileRejections);
 
-  // Initialize Algolia client
-  const searchClient = algoliasearch("W0VJU3M8Q7", "41486e21b0254ba83fa465a7bf0ead54");
-
   const handleUpload = () => {
     uploadData(uploadedImage, labels, textAnnotations, setIsUploading, setUploadedImage, resetAnalysis, resetLabels, setFileRejections);
   };
 
+  const originalSearchClient = algoliasearch("W0VJU3M8Q7", "41486e21b0254ba83fa465a7bf0ead54");
+
+  const searchClient = {
+    ...originalSearchClient,
+    search(requests) {
+      if (requests.every(({ params }) => !params.query)) {
+        return Promise.resolve({
+          results: requests.map(() => ({
+            hits: [],
+            nbHits: 0,
+            nbPages: 0,
+            page: 0,
+            processingTimeMS: 0,
+          })),
+        });
+      }
+
+      return originalSearchClient.search(requests);
+    },
+  };
+
   function Hit({ hit }) {
     return (
-      <article className='border border-gray-200 p-4'>
+      <article className='border border-gray-200 p-4 w-32 h-32 md:w-48 md:h-48 lg:w-64 lg:h-64'>
         <img
-          className='w-full h-auto'
+          className="p-2"
           src={hit.imageUrl}
           alt={hit.text}
         />
@@ -42,17 +60,20 @@ export default function Home() {
 
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <div className='w-full my-8 flex flex-col items-center justify-center'>
-        <InstantSearch
-          searchClient={searchClient}
-          indexName='meme data'
-        >
-          <SearchBox className='p-8' />
-          <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
-            <Hits hitComponent={Hit} />
-          </div>
-        </InstantSearch>
-      </div>
+      <InstantSearch
+        searchClient={searchClient}
+        indexName='meme data'
+      >
+        <Configure hitsPerPage={10} />
+        <SearchBox
+          className='p-8'
+          placeholder='Search for memes'
+        />
+
+        <Hits hitComponent={Hit} />
+
+        <Pagination className='custom-pagination' />
+      </InstantSearch>
 
       <div className='my-8'>
         {isAnalyzed ? (
